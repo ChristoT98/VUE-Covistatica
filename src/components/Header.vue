b3d6ebbf786d118f9f81c22af12e8cc45835b267
ChristoT98 <christolakshan@gmail.com>
2022-04-13 11:44:34 +0530

    Finished

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+<header class="text-center bg-red-800 text-white p-4 mb-10">
+  <div class="text-3xl md:text-5xl font-bold mb-3"><i class="fa fa-virus-covid"></i> Covistatica</div>
+  <p>API by <a class="text-red-300" href="https://covid19api.com" target="_blank">covid19api.com</a></p>
+</header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  
+}
+</script>
